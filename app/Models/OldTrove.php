22f57cac8c2e7c594eb5871a7928c1f8b8b65190
Dave Mills <d.e.mills@stats4sd.org>
2024-09-26 14:16:32 +0100

    include tags in old trove conversion

--- app/Models/OldTrove.php
+++ app/Models/OldTrove.php
@@ -4,9 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class OldTrove extends Model
 {
+
+    use SoftDeletes;
+
     protected $connection = 'mysql_old_troves';
     protected $table = 'troves';
 
@@ -17,4 +22,8 @@ class OldTrove extends Model
         'elements_videos' => 'collection',
     ];
 
+    public function tags(): BelongsToMany
+    {
+        return $this->belongsToMany(OldTag::class, 'taggables', 'taggable_id', 'tag_id');
+    }
 }
