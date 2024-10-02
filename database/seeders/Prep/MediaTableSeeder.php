fbf952d8b098e72285a449ec9e12f0bcce652403
Dave Mills <d.e.mills@stats4sd.org>
2024-10-02 16:12:27 +0100

    hotfixes for seeders

---
+++ database/seeders/Prep/MediaTableSeeder.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace Database\Seeders\Prep;
+
+use App\Models\Trove;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Parallax\FilamentComments\Models\FilamentComment;
+use Spatie\MediaLibrary\MediaCollections\Models\Media;
+
+class MediaTableSeeder extends Seeder
+{
+    public function run(): void
+    {
+        // Handle Media table
+        $uuids = collect([]);
+
+        $mediaItems = DB::connection('mysql_old_troves')
+            ->table('media')
+            ->get();
+
+        foreach ($mediaItems as $item) {
+
+            $itemArray = [];
+            foreach ($item as $key => $value) {
+
+                // preserve arrays
+                if (is_string($value) && (str_starts_with($value, '[') || str_starts_with($value, '{'))) {
+                    $itemArray[$key] = json_decode($value, true);
+                    continue;
+                }
+
+                $itemArray[$key] = $value;
+            }
+
+            //rename collections to match new structure
+            if ($itemArray['model_type'] === Trove::class) {
+
+                $itemArray['collection_name'] = match ($itemArray['collection_name']) {
+                    'coverImage' => 'cover_image_en',
+                    'troveFiles' => 'content_en',
+                    default => $itemArray['collection_name'],
+                };
+            }
+
+            $itemArray['disk'] = config('media-library.disk_name');
+            $itemArray['generated_conversions'] = [];
+
+            // if the uuid is identical, we can overwrite the old one
+            if ($uuids->contains($itemArray['uuid'])) {
+                Media::firstWhere('uuid', $itemArray['uuid'])->updateQuietly($itemArray);
+            } else {
+                Media::create($itemArray);
+            }
+
+            $uuids[] = $itemArray['uuid'];
+
+        };
+
+
+        // handle old trove comments
+        $comments = DB::connection('mysql_old_troves')
+            ->table('trove_comments')
+            ->get()
+            ->each(function ($comment) {
+                FilamentComment::create([
+                    'subject_type' => Trove::class,
+                    'subject_id' => $comment->trove_id,
+                    'user_id' => $comment->user_id,
+                    'comment' => $comment->comment,
+                ]);
+            });
+
+    }
+}
--- database/seeders/TestSeeder.php
+++ database/seeders/TestSeeder.php
@@ -18,23 +18,6 @@ class TestSeeder extends Seeder
      */
     public function run(): void
     {
-        $users = DB::connection('mysql_old_troves')
-            ->table('users')
-            ->select('id', 'name', 'email', 'password')
-            ->get();
-
-        foreach ($users as $user) {
-            if ($user->id > 0) {
-
-                User::create([
-                    'id' => $user->id,
-                    'name' => $user->name,
-                    'email' => $user->email,
-                    'password' => $user->password ?? bcrypt('password'),
-                ]);
-            }
-        }
-
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
@@ -44,65 +27,5 @@ class TestSeeder extends Seeder
             'name' => 'Test Two',
             'email' => 'test2@example.com',
         ]);
-
-        // Handle Media table
-        $uuids = collect([]);
-
-        $mediaItems = DB::connection('mysql_old_troves')
-            ->table('media')
-            ->get();
-
-        foreach ($mediaItems as $item) {
-
-            $itemArray = [];
-            foreach ($item as $key => $value) {
-
-                // preserve arrays
-                if (is_string($value) && (str_starts_with($value, '[') || str_starts_with($value, '{'))) {
-                    $itemArray[$key] = json_decode($value, true);
-                    continue;
-                }
-
-                $itemArray[$key] = $value;
-            }
-
-            //rename collections to match new structure
-            if ($itemArray['model_type'] === Trove::class) {
-
-                $itemArray['collection_name'] = match ($itemArray['collection_name']) {
-                    'coverImage' => 'cover_image_en',
-                    'troveFiles' => 'content_en',
-                    default => $itemArray['collection_name'],
-                };
-            }
-
-            $itemArray['disk'] = config('media-library.disk_name');
-            $itemArray['generated_conversions'] = [];
-
-            // if the uuid is identical, we can overwrite the old one
-            if ($uuids->contains($itemArray['uuid'])) {
-                Media::firstWhere('uuid', $itemArray['uuid'])->updateQuietly($itemArray);
-            } else {
-                Media::create($itemArray);
-            }
-
-            $uuids[] = $itemArray['uuid'];
-
-        };
-
-
-        // handle old trove comments
-        $comments = DB::connection('mysql_old_troves')
-            ->table('trove_comments')
-            ->get()
-        ->each(function($comment) {
-            FilamentComment::create([
-               'subject_type' => Trove::class,
-                'subject_id' => $comment->trove_id,
-                'user_id' => $comment->user_id,
-                'comment' => $comment->comment,
-            ]);
-        });
-
     }
 }
