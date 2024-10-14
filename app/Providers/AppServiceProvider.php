668bb0a71134fe9388ac30dad04ef4743ff43b5c
Dave Mills <d.e.mills@stats4sd.org>
2024-10-14 11:38:38 +0100

    allow fallback to any locale

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\ServiceProvider;
+use Spatie\Translatable\Facades\Translatable;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -21,5 +22,10 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Model::unguard();
+
+        // allow the use of any translation if the fallback is not found
+        Translatable::fallback(
+            fallbackAny: true
+        );
     }
 }
