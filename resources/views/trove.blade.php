9718f7c4b7bef18aa1f22cad5216d749690f668f
Ciara <ciara@stats4sd.org>
2025-05-14 17:26:51 +0100

    hides download btn when no files

--- resources/views/trove.blade.php
+++ resources/views/trove.blade.php
@@ -178,16 +178,17 @@
                 @endif
             </div>
 
-            <!-- DOWNLOAD ALL (ZIP) BUTTON -->
-            <div class="flex justify-end mt-12">
-                <a href="{{ route('trove.download.zip', ['slug' => $resource->slug]) }}" class="bg-stats4sd-red text-white hover-effect px-6 py-3 rounded-2xl text-lg font-bold flex items-center">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-8 w-8 mr-2">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="m9 12.75 3 3m0 0 3-3m-3 3v-7.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
-                    </svg>
-                    {{ t("DOWNLOAD ALL (ZIP)") }}
-                </a>
-            </div>
-            
+            @if($mediaFiles->isNotEmpty())
+                <!-- DOWNLOAD ALL (ZIP) BUTTON -->
+                <div class="flex justify-end mt-12">
+                    <a href="{{ route('trove.download.zip', ['slug' => $resource->slug]) }}" class="bg-stats4sd-red text-white hover-effect px-6 py-3 rounded-2xl text-lg font-bold flex items-center">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-8 w-8 mr-2">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="m9 12.75 3 3m0 0 3-3m-3 3v-7.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                        </svg>
+                        {{ t("DOWNLOAD ALL (ZIP)") }}
+                    </a>
+                </div>
+            @endif
         @endif
     </div>
 
