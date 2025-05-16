6190d53e981b8bf1aaf1b2eaefc17c3e966cfc70
Ciara <ciara@stats4sd.org>
2025-05-16 10:01:55 +0100

    updates design of search & filters

--- resources/views/livewire/collections.blade.php
+++ resources/views/livewire/collections.blade.php
@@ -19,91 +19,111 @@
     </div>
 
     <div class="py-8 px-28">
+        <div class="flex px-8 py-8 gap-8">
+        
+            <!-- Sidebar (Search & Filters) -->
+            <div class="w-80 bg-gray-100 p-6 rounded-lg shadow-md self-start">
+                
+                <div class="pb-4">
+                    <div class="pb-4 text-2xl font-bold">{{ t("Search and filter") }}</div>
+                    <div class="divider"></div>
+                </div>
 
-        <div class="relative flex items-center">
-
-            <livewire:search-bar
-                inputClass="flex-grow py-2 pl-12 pr-4  border-2 border-black rounded-full focus:outline-none transition
-                duration-300 focus:border-stats4sd-red focus:ring-1 focus:ring-stats4sd-red"/>
-            
-            <div class="absolute left-3 top-1/2 transform -translate-y-1/2">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="3" stroke="currentColor"
-                    class="w-5 h-5 text-gray-600">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
-                </svg>
-            </div>
+                <!-- Search bar -->
+                <div class="relative flex items-center mb-6">
+                    <livewire:search-bar
+                        inputClass="flex-grow py-2 pl-12 pr-4 border-2 border-black rounded-full focus:outline-none transition
+                        duration-300 focus:border-stats4sd-red focus:ring-1 focus:ring-stats4sd-red"/>
                     
-            <!-- Clear Button -->
-            @if($query)
-            <svg xmlns="http://www.w3.org/2000/svg" wire:click="clearSearch" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="gray"
-                class="absolute right-12 md:right-2 top-1/2 transform -translate-y-1/2 w-5 h-5 cursor-pointer hover:stroke-gray-700 transition-colors duration-200">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
-            </svg>
-            @endif
-        </div>
+                    <div class="absolute left-3 top-1/2 transform -translate-y-1/2">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="3" stroke="currentColor"
+                            class="w-5 h-5 text-gray-600">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
+                        </svg>
+                    </div>
+                    
+                    <!-- Clear Button -->
+                    @if($query)
+                        <svg xmlns="http://www.w3.org/2000/svg" wire:click="clearSearch" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="gray"
+                            class="absolute right-3 top-1/2 transform -translate-y-1/2 w-5 h-5 cursor-pointer hover:stroke-gray-700 transition-colors duration-200">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                        </svg>
+                    @endif
+                </div>
 
-        <!-- Language Selection -->
-        <div class="mt-6 flex items-center space-x-4">
-            <label class="text-lg font-bold">{{ t("Language:") }}</label>
-            <!-- Spanish Option -->
-            <div class="flex items-center">
-                <input type="checkbox" id="spanish" wire:model="selectedLanguages" value="es" wire:change="search" class="mr-2 accent-stats4sd-red" />
-                <label for="spanish">{{ t("Spanish") }}</label>
+                <!-- Language Selection -->
+                <div class="mb-6" x-data="{ openLanguage: true }">
+                    <div class="border-t border-gray-400 my-4"></div>
+                    <div class="flex justify-between items-center cursor-pointer" @click="openLanguage = !openLanguage">
+                        <label class="text-xl font-bold">{{ t("Language:") }}</label>
+                        <svg class="w-5 h-5 transition-transform duration-300" :class="openLanguage ? 'rotate-90' : 'rotate-0'" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M15 19l-7-7 7-7" />
+                        </svg>
+                    </div>
+                    <div class="space-y-2 mt-2" x-show="openLanguage" x-collapse>
+                        <label class="flex items-center">
+                            <input type="checkbox" wire:model="selectedLanguages" value="es" wire:change="search" class="mr-2 accent-stats4sd-red" />
+                            {{ t("Spanish") }}
+                        </label>
+                        <label class="flex items-center">
+                            <input type="checkbox" wire:model="selectedLanguages" value="en" wire:change="search" class="mr-2 accent-stats4sd-red" />
+                            {{ t("English") }}
+                        </label>
+                        <label class="flex items-center">
+                            <input type="checkbox" wire:model="selectedLanguages" value="fr" wire:change="search" class="mr-2 accent-stats4sd-red" />
+                            {{ t("French") }}
+                        </label>
+                    </div>
+                </div>
+
             </div>
-            <!-- English Option -->
-            <div class="flex items-center">
-                <input type="checkbox" id="english" wire:model="selectedLanguages" value="en" wire:change="search" class="mr-2 accent-stats4sd-red" />
-                <label for="english">{{ t("English") }}</label>
-            </div>
-            <!-- French Option -->
-            <div class="flex items-center">
-                <input type="checkbox" id="french" wire:model="selectedLanguages" value="fr" wire:change="search" class="mr-2 accent-stats4sd-red" />
-                <label for="french">{{ t("French") }}</label>
-            </div>
-        </div>
 
         @if($query || !empty($selectedLanguages) )
-            <!-- Display search results -->
-            <div class="pt-8">
-                {{ t("Showing ") . $totalCollections . t(" collections") }}
-                <button wire:click="clearFilters" class="text-gray-500 hover:text-gray-700 underline">
-                    {{ t("Clear Filters") }}
-                </button>
-            </div>
+            <div class="w-3/4">
+                <div class="pt-2">
+                    {{ t("Showing ") . $totalCollections . t(" collections") }}
+                    @if($query || !empty($selectedLanguages) || !empty($selectedResearchMethods))
+                        <button wire:click="clearFilters" class="text-gray-500 hover:text-gray-700 underline">
+                            {{ t("Clear Filters") }}
+                        </button>
+                    @endif
+                </div>
 
-            <!-- Collections Result Cards -->
-            <div id="Collections-content" class="bg-lightgrey py-8 px-16">
-                <div class="container mx-auto">
-                    <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
-                        @foreach ($this->collections as $index => $collection)
-                            <div class="card hover-effect relative flex flex-col justify-between p-6 border border-gray-200 rounded-lg shadow-xl">
-                                
-                                <a href="/collections/{{ $collection->id }}" class="absolute inset-0 z-0"></a>
-                                <!-- Title -->
-                                <p class="text-xl font-bold bg-stats4sd-red text-white">{!! $collection['title'] !!}</p>
+                <!-- Collections Result Cards -->
+                <div id="Collections-content" class="bg-lightgrey py-8 px-16">
+                    <div class="container mx-auto">
+                        <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+                            @foreach ($this->collections as $index => $collection)
+                                <div class="card hover-effect relative flex flex-col justify-between p-6 border border-gray-200 rounded-lg shadow-xl">
+                                    
+                                    <a href="/collections/{{ $collection->id }}" class="absolute inset-0 z-0"></a>
+                                    <!-- Title -->
+                                    <p class="text-xl font-bold bg-stats4sd-red text-white">{!! $collection['title'] !!}</p>
 
-                                <p class="bg-white pt-8 mb-4 flex-grow">
-                                    {{ \Illuminate\Support\Str::limit(html_entity_decode(strip_tags($collection['description']), ENT_QUOTES, 'UTF-8'), 120, '...') }}
-                                </p>
+                                    <p class="bg-white pt-8 mb-4 flex-grow">
+                                        {{ \Illuminate\Support\Str::limit(html_entity_decode(strip_tags($collection['description']), ENT_QUOTES, 'UTF-8'), 120, '...') }}
+                                    </p>
 
-                                <!-- View Button -->
-                                <div class="flex justify-end">
-                                    <button class="hover-effect bg-stats4sd-red text-white text-center py-2 px-8 rounded-lg">
-                                        {{ t("VIEW") }}
-                                    </button>
+                                    <!-- View Button -->
+                                    <div class="flex justify-end">
+                                        <button class="hover-effect bg-stats4sd-red text-white text-center py-2 px-8 rounded-lg">
+                                            {{ t("VIEW") }}
+                                        </button>
+                                    </div>
                                 </div>
-                            </div>
-                        @endforeach
+                            @endforeach
+
+                        </div>
 
                     </div>
-
                 </div>
+
             </div>
 
         @else
             <!-- Display message when no search term -->
-            <div class="text-center text-semibold py-20">
-                {{ t("Type a search term above to find collections") }}
+            <div class="text-center text-semibold py-20 pl-20">
+                {{ t("Use the search bar to find collections") }}
             </div>
 
         @endif
