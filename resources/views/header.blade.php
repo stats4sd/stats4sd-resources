9ff282c177904091a641766f677ce65970bbcab3
Dave Mills <d.e.mills@stats4sd.org>
2025-07-16 14:01:41 +0100

    make Stats4SD Home link reference correct path based on env

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -5,13 +5,13 @@
             {{-- <a href="https://stats4sd.org/">
                 <img src="/images/Stats4SD_logo.png" alt="Stats4SD logo" class="h-4 w-auto">
             </a> --}}
-            <button class="py-2 px-4 bg-stats4sd-red hover:bg-black text-white flex    font-bold rounded-full w-max" href="https://stats4sd.org/">
+            <button class="py-2 px-4 bg-stats4sd-red hover:bg-black text-white flex    font-bold rounded-full w-max" href="{{ config('app.front_end_url') }}">
             <span class="inline"><svg xmlns="http://www.w3.org/2000/svg" class="mr-2" width="20" height="20" fill="white" viewBox="0 0 20 20"><path d="M0 12l9-8v6h15v4h-15v6z"/></svg></span>            Stats4SD Home</button>
         </div>
 
         <!-- Hamburger Menu (visible on small screens) -->
-        <button 
-            class="md:hidden text-gray-800 focus:outline-none" 
+        <button
+            class="md:hidden text-gray-800 focus:outline-none"
             x-on:click="open = !open">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="h-6 w-6">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M4 6h16M4 12h16M4 18h16" />
@@ -55,14 +55,14 @@
                 </li>
             </ul>
         </nav>
-        
+
     </div>
 
     <!-- Nav Items (visible on small screens) -->
-    <div 
-        class="sm:hidden" 
+    <div
+        class="sm:hidden"
         x-show="open"
-        x-on:click.outside="open = false" 
+        x-on:click.outside="open = false"
         style="display: none;">
         <nav class="bg-white text-right">
             <ul class="flex flex-col space-y-2 px-6 pb-4">
