0ca78cc1c266f9b44b3649056d834191d36dc681
Emily <emily@stats4sd.org>
2025-11-19 18:24:37 +0000

    yet more tweaks

--- resources/views/livewire/ifa-hub-browse-resources.blade.php
+++ resources/views/livewire/ifa-hub-browse-resources.blade.php
@@ -15,14 +15,19 @@
 
     <div class="w-full flex justify-center py-6 px-12">
         <div class="grid sm:grid-cols-2 lg:grid-cols-4 xl:grid-cols-5 gap-6 justify-left max-w-7xl px-12">
+             @php $featureCount = 1 @endphp
             @foreach ($this->topics as $t)
+            @if ($featureCount < 11)
+        
                 <label
                     onclick="document.getElementById('results').scrollIntoView({ behavior: 'smooth', block: 'start' })"
-                    class="hover-effect cursor-pointer flex justify-between relative bg-ifa-yellow rounded-full  overflow-hidden group sm:max-w-[20rem] min-w-[14rem] text-black text-sm px-6 py-4">
+                    class="hover-effect cursor-pointer flex justify-between items-center relative bg-ifa-yellow rounded-full  overflow-hidden group sm:max-w-[20rem] min-w-[14rem] text-black text-sm h-20 px-6 py-4">
                     {{ $t['name'] }}
                     <input type="checkbox" class="hidden" wire:model="selectedTopics" value="{{ $t['id'] }}" wire:change="search"/>
                     <svg clip-rule="evenodd" fill-rule="evenodd" stroke-linejoin="round" class="h-6 w-6" stroke-miterlimit="2"  viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="m14.523 18.787s4.501-4.505 6.255-6.26c.146-.146.219-.338.219-.53s-.073-.383-.219-.53c-1.753-1.754-6.255-6.258-6.255-6.258-.144-.145-.334-.217-.524-.217-.193 0-.385.074-.532.221-.293.292-.295.766-.004 1.056l4.978 4.978h-14.692c-.414 0-.75.336-.75.75s.336.75.75.75h14.692l-4.979 4.979c-.289.289-.286.762.006 1.054.148.148.341.222.533.222.19 0 .378-.072.522-.215z" fill-rule="nonzero"/></svg>
                 </label>
+                @php $featureCount++ @endphp
+              @endif
             @endforeach
         </div>
     </div>
