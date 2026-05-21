0e1854a503dc468e7e02d35a1c3f6b926f066269
Dave Mills <d.e.mills@stats4sd.org>
2026-05-21 19:15:51 +0100

    fix collection page: - remove use of echo + simplify

--- resources/views/livewire/collection-troves.blade.php
+++ resources/views/livewire/collection-troves.blade.php
@@ -1,22 +1,19 @@
 <div id="Resources-content" class="bg-lightgrey py-8 px-16">
     <div class="container mx-auto">
-        @if($resources->isNotEmpty())
-        
+        @if ($resources->isNotEmpty())
+
             <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
                 @foreach ($resources as $resource)
-                    @php if (isset($origin)){
-                        echo "<x-resource-result-card :item="$resource" color="ifa-yellow" textcol="black" target="_self" :show-tags="false" origin="ifa"/>"
-
-                    }
-                    else {
-                         echo "<x-resource-result-card :item="$resource"/>"
-                    ;
-                     }
+                    @if (isset($origin))
+                        <x-resource-result-card :item="$resource" color="ifa-yellow" textcol="black" target="_self"
+                            :show-tags="false" origin="ifa" />
+                    @else
+                        <x-resource-result-card :item="$resource" />
+                    @endif
                 @endforeach
             </div>
         @else
-            <p class="text-gray-600">{{ t("No resources available for this collection.") }}</p>
+            <p class="text-gray-600">{{ t('No resources available for this collection.') }}</p>
         @endif
     </div>
 </div>
-:item="$resource" color="ifa-yellow" textcol="black" target="_self" :show-tags="false" origin="ifa"/
